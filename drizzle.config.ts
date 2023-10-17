e57a415fce19406f12d0d37f567f2346607558fd
leejss <goldemshine@gmail.com>
2023-10-17 21:03:55 +0900

    fix: type error

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ export default {
   schema: "./src/app/db/schema.ts",
   driver: "pg",
   dbCredentials: {
-    connectionString: process.env.POSTGRES_URL,
+    connectionString: process.env.POSTGRES_URL!,
     // password: process.env.POSTGRES_PASSWORD,
     // database: process.env.POSTGRES_DATABASE,
     // host: process.env.POSTGRES_HOST,
